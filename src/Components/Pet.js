5af67c2db3992541590d453b8cab5128c304ecb7
Jack Peterson <jlpeterson829@gmail.com>
2018-02-16 13:40:27 -0500

    Add conditional, add some js styles

--- src/Components/Pet.js
+++ src/Components/Pet.js
@@ -3,10 +3,24 @@ import './Pet.css';
 
 const pet = (props) => {
   const { name, age, id } = props.info;
+  const deleteStyle = {
+    backgroundColor: 'red',
+    border: '1px solid #ddd',
+    padding: '8px',
+    marginLeft: '10px',
+    ':hover': {
+      cursor: 'pointer',
+      backgroundColor: 'salmon'
+    },
+    '@media (max-width: 500px)': {
+      color: 'white'
+    }
+  }
   return (
     <div className="Pet">
       <p>I'm little {name} and I'm {age}</p>
-      <input onChange={props.onNameChange} value={name} data-id={`${id}`}/>
+      <input onChange={props.onNameChange} value={name} data-id={`${id}`} />
+      <button>Delete Me</button>
     </div>
   )
 }
